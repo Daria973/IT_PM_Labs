3202e764f9f418decd119d3941823d22e091944c
Daria <6199dasha@gmail.com>
2025-03-16 21:02:40 +0200

    solved merge conflict new_branch and second_branch

--- code.py
+++ code.py
@@ -230,8 +230,4 @@ if __name__ == "__main__":
     root.title("Прогноз погоди")
     app = App(root)
     root.mainloop()
-<<<<<<< HEAD
     print("Hello world!")
-=======
-    print("Hello!")
->>>>>>> new_branch
